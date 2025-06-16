9031d752485f88c08587b996695a149c550ba721
Muhammad Fillah Alfatih <fillahalfatih@users.noreply.github.com>
2025-06-16 20:30:16 +0700

    update footer to use dynamic year in copyright notice

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="mt-5 py-5 text-center border-top">
-    <p>&#169; 2024 - Created with <i class="bi bi-heart-fill text-danger"></i> by <span class="px-2 py-1 btn-primary rounded text-white">Dakabita</span></p>
+    <p>&#169; {{ date('Y') }} - Created with <i class="bi bi-heart-fill text-danger"></i> by <span class="px-2 py-1 btn-primary rounded text-white">Dakabita</span></p>
 </footer>
